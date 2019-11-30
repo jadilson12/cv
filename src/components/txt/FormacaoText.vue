aadd728f0a0e963be1822af71787ac8fdc586904
Jadilson Guedes <jadilson12@gmail.com>
2019-11-30 17:23:10 -0300

    adicionar a página de formação

---
+++ src/components/txt/FormacaoText.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-container class="width-limit-500 text-xs-left" pa-0 pt-2>
+        <v-layout column>
+            <!-- Header -->
+            <v-flex>
+                <h3 class="subhead">{{ header }}</h3>
+            </v-flex>
+            <!-- Content -->
+            <v-flex text-align-left>
+                <ul>{{ content.description }}</ul>
+                <ul class="italic">{{ content.additional }}</ul>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: "formacao-text",
+    props: ["content"],
+    computed: {
+        header() {
+            return `${this.content.institution} | ${this.content.year}`;
+        },
+        logoLink() {
+            // not used at this point
+            return `../../assets/${this.content.logo}`;
+        }
+    }
+};
+</script>
---
+++ src/pages/FormacaoPage.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-container flat text-xs-left class="education-page width-limit-500">
+        <h3 class="head">Graduação</h3>
+        <formacao-text v-for="item in gaduacao" :key="item.title" :content="item"></formacao-text>
+        <v-container class="divider-container" pa-0 pt-3 pb-3>
+            <v-divider></v-divider>
+        </v-container>
+        <h3 class="head">Certificação</h3>
+        <formacao-text v-for="item in certificacao" :key="item.title" :content="item"></formacao-text>
+    </v-container>
+</template>
+
+<script>
+import FormacaoText from "../components/txt/FormacaoText"
+import { education } from '../api/data.json'
+export default {
+    name: "education",
+    components: { FormacaoText},
+    data() {
+        return {
+            gaduacao: education.gaduacao,
+            certificacao: education.certificacao
+        };
+    },
+};
+</script>
+<style>
+    .education-page {
+        padding-top: 0;
+        padding-bottom: 0;
+    }
+</style>
